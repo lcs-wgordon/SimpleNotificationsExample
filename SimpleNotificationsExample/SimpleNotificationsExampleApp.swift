972f65a6a58b559e57a587bd2be17b04bdb18031
Russell Gordon <rgordon@lcs.on.ca>
2025-05-17 05:32:55 -0400

    Initial Commit

---
+++ SimpleNotificationsExample/SimpleNotificationsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNotificationsExampleApp.swift
+//  SimpleNotificationsExample
+//
+//  Created by Russell Gordon on 2025-05-17.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNotificationsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
